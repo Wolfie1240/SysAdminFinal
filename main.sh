8aa615a2726ef1318a588d6ec02b3306b642979b
wolfle <lwolf1240@gmail.com>
2025-06-11 17:59:42 -0700

    update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+terraform init
+terraform apply
+
 echo "Enter full path of key file (ex. ~/.ssh/key.pem)"
 read key_path
 
@@ -9,3 +12,6 @@ cat > ansible/inventory/hosts.ini <<EOF
 [web]
 $IP ansible_user=ec2-user ansible_ssh_private_key_file=$key_path
 EOF
+
+ansible-playbook -i ansible/inventory/hosts.ini ansible/playbook.yml
+
